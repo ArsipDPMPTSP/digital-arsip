2213df9626fd48dca804500a193523dab9e81469
Roni Otvianus Pethan <ronipethan@gmail.com>
2025-09-25 17:05:50 +0800

    Fix (update app)

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_db_connection():
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT"))
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
     
 # ---------------- OAuth helpers ----------------
