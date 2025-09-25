0bcab771ad5e7f0f99a79e3acf613308bb5ac5d1
Roni Otvianus Pethan <ronipethan@gmail.com>
2025-09-25 16:28:03 +0800

    connect flask to railway msql

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_db_connection():
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT", 25378))
+        port=int(os.getenv("MYSQLPORT", 32881))
     )
     
 # ---------------- OAuth helpers ----------------
